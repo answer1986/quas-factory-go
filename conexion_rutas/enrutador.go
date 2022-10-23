ffe47451c2b689dbf87e08f0f742dcd4171e0728
Alvaro Riquelme Villa <apriquelmev@falabella.cl>
2022-10-22 21:30:00 -0300

    servidor de imagenes reparado

--- conexion_rutas/enrutador.go
+++ conexion_rutas/enrutador.go
@@ -5,8 +5,7 @@ import (
 	"text/template"
 )
 
-var plantillas=template.Must(template.ParseGlob("plantillas/*"))
-
+var plantillas = template.Must(template.ParseGlob("plantillas/*"))
 
 func Enrutador() {
 	http.HandleFunc("/", Inicio)
@@ -16,32 +15,40 @@ func Enrutador() {
 	http.HandleFunc("/clientes", Clientes)
 	http.HandleFunc("/ordendetrabajo", Ordendetrabajo)
 	http.HandleFunc("/loginOk", LoginOk)
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
 	http.ListenAndServe(":9090", nil)
+	http.HandleFunc("/romana", Romana)
+
 }
+
 func Inicio(w http.ResponseWriter, r *http.Request) {
-	plantillas.ExecuteTemplate(w,"inicio",nil)
-	}
-	
-	func Login(w http.ResponseWriter, r *http.Request) {
-	plantillas.ExecuteTemplate(w,"login",nil)
-	}
-	
-	func Usuarios(w http.ResponseWriter, r *http.Request) {
-	plantillas.ExecuteTemplate(w,"usuarios",nil)
-	}
-	
-	func Bodega(w http.ResponseWriter, r *http.Request) {
-	plantillas.ExecuteTemplate(w,"bodega",nil)
-	}
-	
-	func Clientes(w http.ResponseWriter, r *http.Request) {
-	plantillas.ExecuteTemplate(w,"clientes",nil)
-	}
-	
-	func Ordendetrabajo(w http.ResponseWriter, r *http.Request) {
-	plantillas.ExecuteTemplate(w,"orden de trabajo",nil)
-	}
-	
-	func LoginOk(w http.ResponseWriter, r *http.Request) {
-	plantillas.ExecuteTemplate(w,"loginOk",nil)
-	}
+	plantillas.ExecuteTemplate(w, "inicio", nil)
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	plantillas.ExecuteTemplate(w, "login", nil)
+}
+
+func Usuarios(w http.ResponseWriter, r *http.Request) {
+	plantillas.ExecuteTemplate(w, "usuarios", nil)
+}
+
+func Bodega(w http.ResponseWriter, r *http.Request) {
+	plantillas.ExecuteTemplate(w, "bodega", nil)
+}
+
+func Clientes(w http.ResponseWriter, r *http.Request) {
+	plantillas.ExecuteTemplate(w, "clientes", nil)
+}
+
+func Ordendetrabajo(w http.ResponseWriter, r *http.Request) {
+	plantillas.ExecuteTemplate(w, "orden de trabajo", nil)
+}
+
+func LoginOk(w http.ResponseWriter, r *http.Request) {
+	plantillas.ExecuteTemplate(w, "loginOk", nil)
+}
+
+func Romana(w http.ResponseWriter, r *http.Request) {
+	plantillas.ExecuteTemplate(w, "romana", nil)
+}
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 	"quasfactory/conexion_rutas"
-	"quasfactory/metodos"
+	
 )
 
 
@@ -13,11 +13,9 @@ import (
 func main() {
 
 	conexion_rutas.ConexionBD()
-	metodos.Login()
-	
-	
 	conexion_rutas.Enrutador()
 	log.Println("Servidor corriendo en http://localhost:9090")
+	
 
 	
 }
